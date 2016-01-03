8d15cfd08c7600ff0415b40926cb2fc41cc9bef3
Milos Gajdos <milosgajdos83@gmail.com>
2016-01-03 02:53:27 +0000

    Feature Normalization for multivariate Linear Regression

--- featureNormalize.m
+++ featureNormalize.m
@@ -25,14 +25,11 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
-
-
+for i = 1:size(X,2)
+    mu(i) = mean(X(:,i));
+    sigma(i) = std(X(:,i));
+    X_norm(:,i) = (X(:,i) - mu(i))/sigma(i);
+end
 
 % ============================================================
 
