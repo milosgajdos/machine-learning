a23ddfbc83874d2740deca30261663abdf8ff664
Milos Gajdos <milosgajdos83@gmail.com>
2016-01-28 17:22:11 +0000

    Added polynomial features to the model.

--- week5-model-diagnostics/ex5/polyFeatures.m
+++ week5-model-diagnostics/ex5/polyFeatures.m
@@ -5,7 +5,6 @@ function [X_poly] = polyFeatures(X, p)
 %   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
 %
 
-
 % You need to return the following variables correctly.
 X_poly = zeros(numel(X), p);
 
@@ -15,10 +14,9 @@ X_poly = zeros(numel(X), p);
 %
 % 
 
-
-
-
-
+for i = 1:p
+    X_poly(:, i) = X.^i;
+end
 
 % =========================================================================
 
